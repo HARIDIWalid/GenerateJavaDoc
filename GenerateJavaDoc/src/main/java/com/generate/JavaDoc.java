d050892a85334cb10df59f18da97de7643157f53
Walid <w.haridi@gmail.com>
2020-04-14 17:15:39 +0200

    :tada: Initial commit

---
+++ GenerateJavaDoc/src/main/java/com/generate/JavaDoc.java
@@ -0,0 +1,20 @@
+package com.generate;
+
+/**
+ * <b>Class TeamPlanningUpdateService</b>
+ * <br><br>
+ * Provides all update web services for project [team-planning-content].
+ * 
+ * @author Walid HARIDI (Sopra HR Software)
+ */
+public class JavaDoc {
+	
+	/**
+	 * Convrt number to string
+	 * @param {@link Number} numberOfWeek
+	 * @return {@link String} numberOfWeek to String format
+	 */
+	public String updateDay(int numberOfWeek) throws Exception {
+		return "" + numberOfWeek;
+	}
+}
